588f03be444136d4e44f146bca30375aff764a51
abdoemi <MAHJOUBI.ABDESSAMAD@student.emi.ac.ma>
2023-05-31 09:22:14 +0100

    my commit

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -7,7 +7,7 @@ function ProfileCard({title,handle,image}) {
         <div className="card border-primary mb-3" style={{width : '18rem'}}>
             <img className="card-img-top" src={image} alt="Profile" />
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+                <p className="card-title">{title}</p>
                 <p className="card-text">{handle}</p>
             </div>
         </div>
